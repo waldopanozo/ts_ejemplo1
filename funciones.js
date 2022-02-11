cf602339d9421f03bd66b0fb62ba0b0ddab123cc
Waldo <waldo.panozo@gmail.com>
2022-02-11 19:40:52 -0400

    Uso de variables

--- funciones.js
+++ funciones.js
@@ -1,5 +1,14 @@
 "use strict";
+var message = "Hey, how's it going! Cool!";
+//let n:number = 4;
+var n = 4;
+var annnny;
+annnny = "Hello";
+annnny = 24.34;
+var PI = 3.14;
 function sayHello() {
-    console.log("Hey, how's it going! Cool!");
+    console.log(message);
+    console.log(n);
+    console.log(PI);
 }
 sayHello();
--- funciones.ts
+++ funciones.ts
@@ -1,4 +1,18 @@
+let message:string = "Hey, how's it going! Cool!";
+//let n:number = 4;
+let n = 4;
+
+let annnny:any;
+
+annnny = "Hello";
+annnny = 24.34;
+
+
+const PI = 3.14;
+
 function sayHello(){
-    console.log("Hey, how's it going! Cool!");
+    console.log(message);
+    console.log(n);
+    console.log(PI);
 }
 sayHello();
